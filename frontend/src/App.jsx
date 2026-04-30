d1c8acb89079e224da8cbe4214c4ce5c576c4167
Jericho Rosell <jerichorosell1@gmail.com>
2026-04-30 00:43:59 -0700

    Implement LeafletMap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
+import LeafletMap from "./Map.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <button type="button" className="text-white bg-blue-600 box-border border border-transparent hover:bg-blue-700 shadow-xs font-medium leading-5 rounded-lg text-sm px-4 py-2.5 focus:outline-none">Default</button>
+      <button
+        type="button"
+        className="text-white bg-blue-600 box-border border border-transparent hover:bg-blue-700 shadow-xs font-medium leading-5 rounded-lg text-sm px-4 py-2.5 focus:outline-none"
+      >
+        Default
+      </button>
+      <LeafletMap />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
