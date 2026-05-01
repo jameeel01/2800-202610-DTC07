21a1a2b1a38b760f62ea6235299e00261e962859
Jericho Rosell <jerichorosell1@gmail.com>
2026-04-30 22:22:08 -0700

    Change Map Height

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,6 @@ import LeafletMap from "./Map.jsx";
 function App() {
   return (
     <>
-      <button
-        type="button"
-        className="text-white bg-blue-600 box-border border border-transparent hover:bg-blue-700 shadow-xs font-medium leading-5 rounded-lg text-sm px-4 py-2.5 focus:outline-none"
-      >
-        Default
-      </button>
       <LeafletMap />
     </>
   );
