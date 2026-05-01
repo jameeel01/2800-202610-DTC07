8f604e1bc3c46dbd9f36701f49ef425205107c80
Jericho Rosell <jerichorosell1@gmail.com>
2026-04-30 22:33:02 -0700

    Fix app.jsx unused imports and duplicate app function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "./assets/vite.svg";
 // import heroImg from "./assets/hero.png";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import { BrowserRouter } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import { BrowserRouter } from "react-router-dom";
 
-function App() {
-  // const [count, setCount] = useState(0);
-
-  return (
-    <BrowserRouter>
-      <Navbar />
-    </BrowserRouter>
-=======
 import "./App.css";
 import LeafletMap from "./Map.jsx";
 
@@ -22,7 +14,6 @@ function App() {
     <>
       <LeafletMap />
     </>
->>>>>>> leaflet
   );
 }
 
