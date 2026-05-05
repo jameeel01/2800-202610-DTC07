035f16d6d32554d87bb2de70abd1c875bdf7aee3
Jericho Rosell <jerichorosell1@gmail.com>
2026-05-05 08:03:14 -0700

    Move map to components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,31 @@
 import "./App.css";
 import LandingPage from "./pages/landingPage";
-import LeafletMap from "./Map.jsx";
+import LeafletMap from "./components/Map.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                {
-                    // Pages that do not need the navbar
-                }
-                <Route path="/" element={<LandingPage />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        {
+          // Pages that do not need the navbar
+        }
+        <Route path="/" element={<LandingPage />} />
 
-                {
-                    // Pages that do need the navbar
-                }
-                <Route element={<MainLayout />}>
-                    <Route path="/map" element={<LeafletMap />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+        {
+          // Pages that do need the navbar
+        }
+        <Route element={<MainLayout />}>
+          <Route path="/map" element={<LeafletMap />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Map.jsx
@@ -0,0 +1,54 @@
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useState } from "react";
+
+function ClickHandler() {
+  const [position, setPosition] = useState(null); // Location container
+  useMapEvents({
+    click(e) {
+      setPosition(e.latlng);
+    },
+  });
+
+  if (position === null) {
+    return null;
+  } else
+    return (
+      <Marker position={position}>
+        <Popup>
+          Latitude: {position.lat} <br></br>
+          Longitude: {position.lng} <br></br>
+          Temperature Drop: {"-2.4°C"} <br></br>
+          Number Of Plantable Trees: {"3 Trees"} <br></br>
+          Area Of Shade Coverage: {"160 m²"}
+        </Popup>
+      </Marker>
+    );
+}
+
+function LeafletMap() {
+  return (
+    <div style={{ height: "calc(100vh - 80px)" }}>
+      <MapContainer
+        center={[49.24966, -123.11934]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <ClickHandler />
+      </MapContainer>
+    </div>
+  );
+}
+
+export default LeafletMap;
