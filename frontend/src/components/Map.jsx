e6d6f32a3ba9d79de12759d69ca0f2f095f3d6ac
Jericho Rosell <jerichorosell1@gmail.com>
2026-05-05 22:42:51 -0700

    Seperate files into seperate components

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,88 +1,13 @@
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  useMapEvents,
-} from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { useState } from "react";
-import L from "leaflet";
-import BlueMarker from "../assets/BlueMarker.svg";
 import HeatmapLayer from "./HeatMapLayer";
-
-const bluemarker = L.icon({
-  iconUrl: BlueMarker,
-  iconSize: [38, 48],
-  iconAnchor: [19, 48],
-  popupAnchor: [0, -38],
-});
-
-function ClickHandler({ isPinDropMode }) {
-  const [position, setPosition] = useState(null); // Location Container
-  useMapEvents({
-    click(e) {
-      if (!isPinDropMode) return;
-      setPosition(e.latlng);
-    },
-  });
-
-  if (position === null) {
-    return null;
-  } else
-    return (
-      <Marker position={position} icon={bluemarker}>
-        <Popup>
-          Latitude: {position.lat} <br></br>
-          Longitude: {position.lng} <br></br>
-          Temperature Drop: {"-2.4°C"} <br></br>
-          Number Of Plantable Trees: {"3 Trees"} <br></br>
-          Area Of Shade Coverage: {"160 m²"}
-        </Popup>
-      </Marker>
-    );
-}
+import ClickHandler from "./ClickHandler";
+import PinDropBanner from "./PinDropBanner";
+import NominateButton from "./NominateButton";
 
 function LeafletMap({ isPinDropMode, setIsPinDropMode }) {
   return (
     <div style={{ height: "calc(100vh - 80px)" }}>
-      {isPinDropMode && ( // Popup Banner
-        <div
-          style={{
-            position: "absolute",
-            top: "16px",
-            left: "50%",
-            transform: "translateX(-50%)",
-            zIndex: 1000,
-            background: "#111827",
-            padding: "10px 20px",
-            borderRadius: "8px",
-            display: "flex",
-            gap: "16px",
-            alignItems: "center",
-            fontWeight: "bold",
-            boxShadow: "0 2px 8px rgba(0,0,0,0.3)",
-          }}
-        >
-          <span style={{ color: "#9ca3af" }}>
-            Tap the map to place your pin. To confirm, click on the marker.
-          </span>
-
-          <button
-            onClick={() => setIsPinDropMode(false)}
-            style={{
-              background: "none",
-              color: "#9ca3af",
-              border: "none",
-              fontSize: "18px",
-              cursor: "pointer",
-            }}
-          >
-            ✕
-          </button>
-        </div>
-      )}
-
       <MapContainer // Map Centered On Van
         center={[49.2827, -123.1207]}
         zoom={13}
@@ -94,6 +19,10 @@ function LeafletMap({ isPinDropMode, setIsPinDropMode }) {
           setIsPinDropMode={setIsPinDropMode}
         />
 
+        <PinDropBanner
+          isPinDropMode={isPinDropMode}
+          setIsPinDropMode={setIsPinDropMode}
+        ></PinDropBanner>
         <HeatmapLayer></HeatmapLayer>
 
         <TileLayer // Actual Map
@@ -101,24 +30,7 @@ function LeafletMap({ isPinDropMode, setIsPinDropMode }) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
       </MapContainer>
-
-      <button // Nomination Button
-        onClick={() => setIsPinDropMode(true)}
-        style={{
-          position: "relative",
-          bottom: "70px",
-          right: "-1570px",
-          zIndex: 1000,
-          padding: "10px 18px",
-          background: "#1a1a2e",
-          color: "#9ca3af",
-          borderRadius: "8px",
-          cursor: "pointer",
-          fontWeight: "bold",
-        }}
-      >
-        Nominate +
-      </button>
+      <NominateButton setIsPinDropMode={setIsPinDropMode}></NominateButton>
     </div>
   );
 }
